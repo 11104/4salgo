b15a951ba7bb95f3265d344c09dacb2e8d19c493
11104 <nagai606.1201@gmail.com>
2022-11-30 18:31:56 +0900

    add s_in

--- renketsu.cpp
+++ renketsu.cpp
@@ -1,23 +1,64 @@
 #include <stdio.h>
+#include <string>
 #include <iostream>
+#include <locale.h>
+#include <wchar.h>
 using namespace std;
 
-//typedef node
-struct node{
-    char key[26];
-    struct node *next_char[26];
-};
+//typedef snode
+typedef struct s_node{
+    const char* skey;
+    const char* smean;
+    struct s_node *snext;
+}snode;
+snode* shead=NULL;
+
+//typedef tnode
+typedef struct t_node{
+    const char* tkey;
+    const char* tmean;
+    struct t_node *tnext[26];
+}tnode;
+tnode* thead=NULL;
 
 //convert char to int
 int numchar(char ch){
     int henkan;
     henkan=ch;
-        return henkan-96;
+    return henkan-97;
+}
+
+//intsert to s
+static void s_in(const char* sc/*,string sm*/){
+    snode* stmp;
+    stmp=(snode*)malloc(sizeof(snode));
+    stmp->skey=sc;
+    string sa;
+    std::cin>>sa;
+    stmp->smean=sa;
+    stmp->snext=NULL;
+    shead=stmp;
+}
+
+//print s
+void prints(){
+    snode *stmp;
+    for(stmp=shead;stmp->snext!=NULL;stmp=stmp->snext){
+        std::cout<<stmp->skey<<" ";
+        std::wcout<<stmp->smean<<std::endl;
+    }
 }
 
 //main
+void swstr(const char *){};
+
 int main(){
-    int p=numchar('a');
-    std::cout<<p<<std::endl;
+    setlocale(LC_ALL, "ja_JP.UTF-8");
+    s_in("apple");
+    s_in("pencil");
+
+    prints();
+
+    
     return 0;
 }
