d5928193b9699a391270b6eb02ee71aca5768681
11104 <nagai606.1201@gmail.com>
2022-11-30 16:16:45 +0900

    add .gitignore

---
+++ renketsu.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+//typedef node
+struct node{
+    char key[26];
+    struct node *next_char[26];
+};
+
+//convert char to int
+int numchar(char ch){
+    int henkan;
+    henkan=ch;
+    return henkan-96;
+}
+
+//main
+int main(){
+    int p=numchar('a');
+    std::cout<<p<<std::endl;
+    return 0;
+}
